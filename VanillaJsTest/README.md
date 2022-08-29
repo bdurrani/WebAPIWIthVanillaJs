3961dba9d6d9d03374d4266e629582f5bf546f74
Bilal Durrani <bilal.durrani@xero.com>
2022-08-28 21:51:00 -0400

    save readme

---
+++ VanillaJsTest/README.md
@@ -0,0 +1,4 @@
+## Introduction
+An example of how to use vanilla js with a web api project. 
+
+Also incorporate nodejs modules so that I'm not writing everything from scatch. 
---
+++ VanillaJsTest/wwwroot/webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports ={
+    entry: './src/index.js',
+    output: {
+        filename: "build.js",
+        path: path.resolve(__dirname, 'dist')
+    }
+}
