d70311ada3de6e6f6e85dc92bfef17977e39dc6b
Bilal Durrani <bilal.durrani@xero.com>
2022-09-04 17:08:13 -0400

    got file upload to work

--- VanillaJsTest/wwwroot/src/index.js
+++ VanillaJsTest/wwwroot/src/index.js
@@ -1,24 +1,35 @@
-import { camelCase} from "lodash";
+import {camelCase} from "lodash";
 
-console.log(camelCase("hello world"));
 
-const form = document.forms.namedItem("upload-form");
+(async () => {
 
-form.addEventListener(
-    "submit",
-    async (event) => {
-        const output = document.querySelector("output");
-        const formData = new FormData(form);
+    console.log(camelCase("hello world"));
 
-        formData.append("CustomField", "This is some extra data");
+    const form = document.forms.namedItem("upload-form");
 
-        event.preventDefault();
-        const response = await fetch("weatherforecast",
-            {
-                body: formData,
-                method: "post"
-            });
-        return;
-    },
-    false
-);
+    form.addEventListener(
+        "submit",
+        async (event) => {
+            const output = document.querySelector("output");
+            const formData = new FormData(form);
+
+            formData.append("CustomField", "This is some extra data");
+
+            event.preventDefault();
+            const response = await fetch("/weatherforecast",
+                {
+                    body: formData,
+                    method: "POST",
+                });
+            if (!response.ok) {
+                throw new Error(`HTTP error, status = ${response.status}`);
+            }
+            const blob = await response.blob();
+            const myImage = document.querySelector(".my-image");
+            const prefix = "data:image/png;base64";
+            const txt =await blob.text();
+            myImage.src = `${prefix}, ${txt}`;
+        },
+        false
+    );
+})();
