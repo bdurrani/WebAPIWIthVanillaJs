9890973899d58f90fc4097e1f634fda438c40a8a
Bilal Durrani <bilal.durrani@xero.com>
2022-09-04 16:18:36 -0400

    try post form using js

--- VanillaJsTest/wwwroot/src/index.js
+++ VanillaJsTest/wwwroot/src/index.js
@@ -1,3 +1,24 @@
 import { camelCase} from "lodash";
 
 console.log(camelCase("hello world"));
+
+const form = document.forms.namedItem("upload-form");
+
+form.addEventListener(
+    "submit",
+    async (event) => {
+        const output = document.querySelector("output");
+        const formData = new FormData(form);
+
+        formData.append("CustomField", "This is some extra data");
+
+        event.preventDefault();
+        const response = await fetch("weatherforecast",
+            {
+                body: formData,
+                method: "post"
+            });
+        return;
+    },
+    false
+);
