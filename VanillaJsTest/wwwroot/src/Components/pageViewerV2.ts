89e4733f4828a04014c149169e59f93efd6c15d2
Bilal Durrani <bilal33@gmail.com>
2022-09-16 14:08:18 -0400

    page looks nicer

--- VanillaJsTest/wwwroot/src/Components/pageViewerV2.ts
+++ VanillaJsTest/wwwroot/src/Components/pageViewerV2.ts
@@ -1,38 +1,31 @@
-import {css, html, LitElement} from "lit";
-import {customElement, property} from "lit/decorators.js";
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement("page-viewer-v2")
 export class PageViewerV2 extends LitElement {
-    @property()
-    version = "STARTING";
+  @property({ attribute: false })
+  imageData: string;
 
-    @property({attribute: false})
-    imageData: string;
+  @property()
+  src: string;
 
-    @property()
-    src: string;
+  constructor() {
+    super();
+    this.imageData = "";
+    this.src = "";
+  }
 
-    constructor() {
-        super();
-        this.imageData = "";
-        this.src = "";
-    }
-
-    static styles = css`
+  static styles = css`
     .statement-page {
       color: green;
     }
   `;
 
-    render() {
-        const imageTmpl = this.src ?
-            html`
-          <img src=${this.src} alt="Visit the MDN site"> ` : ``;
+  render() {
+    const imageTmpl = this.src
+      ? html` <img src=${this.src} alt="Visit the MDN site" /> `
+      : ``;
 
-        return html`
-      <div class="statement-page">
-        ${imageTmpl}
-      </div>
-    `;
-    }
+    return html` <div class="statement-page">${imageTmpl}</div> `;
+  }
 }
