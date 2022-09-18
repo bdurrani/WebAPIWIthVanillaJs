ec083a841ff589b8e762eed96a62af74d760c2ee
Bilal Durrani <bilal33@gmail.com>
2022-09-18 16:34:44 -0400

    detect img update

--- VanillaJsTest/wwwroot/src/Components/pageViewerV2.ts
+++ VanillaJsTest/wwwroot/src/Components/pageViewerV2.ts
@@ -1,7 +1,7 @@
-import {css, html, LitElement, PropertyValues} from "lit";
+import { css, html, LitElement, PropertyValues } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import {query} from 'lit/decorators/query.js';
-import { v4 as uuidv4 } from 'uuid';
+import { query } from "lit/decorators/query.js";
+import { v4 as uuidv4 } from "uuid";
 
 const imageIdPrefix = `statement-image-`;
 @customElement("page-viewer-v2")
@@ -12,7 +12,7 @@ export class PageViewerV2 extends LitElement {
   @property()
   src: string;
 
-  @query('#statement-image')
+  @query("#statement-image")
   private _first!: HTMLImageElement;
 
   constructor() {
@@ -30,27 +30,33 @@ export class PageViewerV2 extends LitElement {
   render() {
     const imageId = `${imageIdPrefix}${uuidv4()}`;
     const imageTmpl = this.src
-      ? html` <img src=${this.src} id="statement-image" alt="statement page" /> `
+      ? html`
+          <img src=${this.src} id="statement-image" alt="statement page" />
+        `
       : ``;
 
     return html` <div class="statement-page">${imageTmpl}</div> `;
   }
 
   // https://stackoverflow.com/a/67499234/293611
-  async getUpdateComplete(): Promise<boolean>{
-    await super.getUpdateComplete()
-    const children = this.renderRoot.querySelectorAll('*');
-    // await Promise.all(Array.from(children).map((c) => c.updateComplete));
-    const marginElements = this._first; // Array.from(this.shadowRoot.querySelectorAll('margin-element'))
-    // await Promise.all(marginElements.map(el => el.updateComplete))
-    return true
+  override async getUpdateComplete(): Promise<boolean> {
+    const result = await super.getUpdateComplete();
+    const imageLoaded = new Promise((res) => {
+      this._first.onload = function () {
+        res(null);
+      };
+    });
+    await imageLoaded;
+    return result;
   }
 
-  updated(changedProperties: PropertyValues) {
-
-    const children = this.renderRoot.querySelectorAll('*');
-    const test = this._first;
+  async updated(changedProperties: PropertyValues) {
+    await this.updateComplete;
     const bounds = this._first.getBoundingClientRect();
     console.log(JSON.stringify(bounds));
+    // test.onload = () => {
+    //   const onloadbounds = this._first.getBoundingClientRect();
+    //   console.log(JSON.stringify(onloadbounds));
+    // };
   }
 }
